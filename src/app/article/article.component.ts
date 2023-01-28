3e775b3c97d23e400e483b503214348405602fc6
midouwell <midouwell@gmail.com>
2023-01-28 02:50:22 -0500

    update changes

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostBinding, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -8,22 +9,34 @@ import { Component, HostBinding, OnInit } from '@angular/core';
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-  votes: number;
-  title: string;
-  link: string;
+  article: Article;
+
 
   constructor() {
-    this.title = 'Angular';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+
+    this.article = new Article(
+      'Angular',
+      'http://angular.io',
+      10);
   }
 
-  voteUp() {
-    this.votes += 1;
+  voteUp(event: any) {
+    //this.votes += 1;
+    this.article.voteUp();
+    return false;
   }
+
+  // @HostListener('click', ['$event'])
+  // voteUp(event: any) {
+  //   // event handler code here
+  //   console.log(event);
+  //   this.votes += 1;
+  //   return false;
+  // }
 
   voteDown() {
-    this.votes -= 1;
+    this.article.voteDown();
+    return false;
   }
 
 
